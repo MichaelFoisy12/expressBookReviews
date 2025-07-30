c9406a5ac3ddda3a757b9a68e242c8eaa35b7c39
michaelFoisy12 <michaelwfoisy@gmail.com>
2025-07-30 12:17:31 -0400

    Updated file

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -18,14 +18,27 @@ public_users.get('/',function (req, res) {
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // ISBN Number
+  const isbn = req.params.isbn;
+    
+    if (books[isbn]) {
+        res.send(JSON.stringify(books[isbn], null, 4));
+        return res.status(200).json({message: "Book details fetched successfully", book: books[isbn]});
+    } else {
+        res.status(404).json({message: "Book not found"});
+    }
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const author = req.params.author;
+  let booksByAuthor = Object.values(books).filter(book => book.author.toLowerCase() === author.toLowerCase());
+  if (booksByAuthor.length > 0) {
+    res.send(JSON.stringify(booksByAuthor, null, 4));
+    return res.status(200).json({message: "Books by author fetched successfully", books: booksByAuthor});
+  } else {
+    return res.status(404).json({message: "No books found by this author"});
+  }
 });
 
 // Get all books based on title
